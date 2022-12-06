8229638e76d3dff58050d1b435b560719aacc3c9
lkwinta <lukaszmind@gmail.com>
2022-12-06 12:13:45 +0100

    day 5 and day 6

--- day5/main.cpp
+++ day5/main.cpp
@@ -1,22 +1,96 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
+#include <queue>
+#include <map>
 
 using namespace std;
 
+vector<string> split(const string& str, char delimiter){
+    vector<string> result;
+    size_t pos = str.find(delimiter);
+    size_t pos_old = 0;
+
+    while(pos != string::npos){
+        string s = str.substr(pos_old, pos - pos_old);
+        //if(s.length() != 0)
+            result.push_back(s);
+
+        pos_old = pos + 1;
+        pos = str.find(delimiter, pos_old);
+    }
+    result.push_back(str.substr(pos_old, str.length() - pos_old));
+    return result;
+}
+
+struct command{
+    char from;
+    int how_many;
+    char where;
+};
+
 int main() {
     //input file
-    ifstream data("test.txt");
+    ifstream data("data.txt");
 
     string s_line;
+    bool if_stack = false;
+
+    queue<command> qc_commands;
+    vector<string> vs_stack;
+    map<char, vector<char>> mc_stack;
+    vector<string> vs_command;
 
     while(!data.eof()){
-        if(data){
-            data >> s_line;
-
+        getline(data, s_line);
+        if(!s_line.empty()){
+            if(!if_stack){
+                vs_stack.push_back(s_line);
+            } else {
+                vs_command = split(s_line, ' ');
+                qc_commands.push({.from = vs_command[3][0], .how_many=stoi(vs_command[1]), .where = vs_command[5][0]});
+            }
+        } else {
+            if_stack = true;
         }
     }
+
+    for(int column = (int)vs_stack[vs_stack.size() - 1].length(); column >= 0; column--) {
+        if(vs_stack.back()[column] != ' ' && vs_stack.back()[column] != '\0') {
+            for (int row = (int)vs_stack.size() - 2; row >= 0; row--) {
+                if (vs_stack[row].length() > column) {
+                    if(vs_stack[row][column] != ' ')
+                        mc_stack[vs_stack.back()[column]].push_back(vs_stack[row][column]);
+                }
+            }
+        }
+    }
+
+    while(!qc_commands.empty()){
+        command c = qc_commands.front();
+        int size = (int)mc_stack[c.from].size();
+
+        /*part1
+        for(int i = size - -1; i >= size - c.how_many; i--){
+            mc_stack[c.where].push_back(mc_stack[c.from][i]);
+        }
+        */
+
+        //part2
+        for(int i = size - c.how_many; i < size; i++){
+            mc_stack[c.where].push_back(mc_stack[c.from][i]);
+        }
+        mc_stack[c.from].erase(mc_stack[c.from].end() - c.how_many, mc_stack[c.from].end());
+        qc_commands.pop();
+    }
+
+    string output;
+
+    for(pair<char, vector<char>> pair : mc_stack){
+        output += pair.second.back();
+    }
+
+    cout << output << endl;
 
     data.close();
 
--- day6/main.cpp
+++ day6/main.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
+#include <set>
+
 
 using namespace std;
 
+struct fourth{
+    char a;
+    char b;
+    char c;
+    char d;
+};
+
+bool is_distinct(fourth f){
+    return f.a != f.b && f.a != f.c && f.a != f.d && f.b != f.c && f.b != f.d && f.c != f.d;
+}
+
+bool is_distinct(const char* tab, int len){
+    set<char> s;
+    for(int i = 0; i < len; i++) {
+        s.insert(tab[i]);
+    }
+    if(s.size() == len)
+        return true;
+    return false;
+}
+
 int main() {
     //input file
-    ifstream data("test.txt");
+    ifstream data("data.txt");
 
-    string s_line;
+    string s_input;
 
-    while(!data.eof()){
-        if(data){
-            data >> s_line;
+    data >> s_input;
+    fourth f = {s_input[0], s_input[1], s_input[2], s_input[3]};
+    for(size_t i = 4; i < s_input.length(); i++){
+        f.a = f.b;
+        f.b = f.c;
+        f.c = f.d;
+        f.d = s_input[i];
+        if(is_distinct(f)){
+            cout << i + 1 << endl;
+            break;
+        }
+    }
+    const char* tab;
 
+    for(size_t i = 14; i < s_input.length(); i++){
+        tab = s_input.substr(i - 14, 14).c_str();
+        if(is_distinct(tab, 14)){
+            cout << i << endl;
+            break;
         }
     }
 
