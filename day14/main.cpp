27a9ff2eb6075dc4e3fda0b2a9121ac46bb7a8a8
lkwinta <lukaszmind@gmail.com>
2022-12-14 17:08:55 +0100

    day14

--- day14/main.cpp
+++ day14/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <iomanip>
+#include <windows.h>
 
 using namespace std;
 
@@ -12,10 +14,28 @@ int main() {
 
     string s_line;
 
-    while(!data.eof()){
+   /* while(!data.eof()){
         if(data){
             data >> s_line;
         }
+    }*/
+
+    string tab[5];
+    tab[0] = "................................................";
+    tab[1] = "................................................";
+    tab[2] = "................................................";
+    tab[3] = "................................................";
+    tab[4] = "................................................";
+
+    for(int i = 0; i < tab[0].length(); i++){
+        tab[2][i] = 'o';
+        for(int j = 0; j < 5; j++){
+            std::cout << tab[j] << endl;
+        }
+        Sleep(500);
+        std::cout << std::flush;
+        tab[2][i] = '.';
+
     }
 
     data.close();
